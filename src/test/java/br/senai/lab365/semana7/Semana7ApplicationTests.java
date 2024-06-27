3b96d01680729fd2a098ffbe8d93cfdd4f6e48d5
André <andrejusmattos@gmail.com>
2024-06-27 15:41:11 -0300

    Primeiro commit

---
+++ src/test/java/br/senai/lab365/semana7/Semana7ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.lab365.semana7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
