ac8fe2dd933d472418cf36474d0285fd8c3a4e42
André <andrejusmattos@gmail.com>
2024-06-27 15:51:04 -0300

    Criação NutricionistaEntity

---
+++ src/main/java/br/senai/lab365/semana7/entities/NutricionistaEntity.java
@@ -0,0 +1,18 @@
+package br.senai.lab365.semana7.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Nutricionista")
+public class NutricionistaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String CRN;
+    private String especialidade;
+
+}
