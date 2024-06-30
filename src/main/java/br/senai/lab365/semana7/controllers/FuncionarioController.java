f9fc4c782f3ef2df7049c5e27d8073b318b11e51
André <andrejusmattos@gmail.com>
2024-06-30 18:18:47 -0300

    Implementação da loógica de FuncionarioController

--- src/main/java/br/senai/lab365/semana7/controllers/FuncionarioController.java
+++ src/main/java/br/senai/lab365/semana7/controllers/FuncionarioController.java
@@ -1,10 +1,44 @@
 package br.senai.lab365.semana7.controllers;
 
+import br.senai.lab365.semana7.controllers.dtos.FuncionarioRequestDTO;
+import br.senai.lab365.semana7.controllers.dtos.FuncionarioResponseDTO;
+import br.senai.lab365.semana7.services.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/funcionario")
+@RequestMapping("/funcionarios")
 public class FuncionarioController {
+
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+    @PostMapping
+    public FuncionarioResponseDTO criarFuncionario (@RequestBody FuncionarioRequestDTO requestDTO) {
+        return funcionarioService.criarFuncionario(requestDTO);
+    }
+
+    @GetMapping
+    public List<FuncionarioResponseDTO> listarTodosFuncionarios () {
+        return funcionarioService.listarTodosFuncionarios();
+    }
+
+    @GetMapping("/{id}")
+    public FuncionarioResponseDTO listarFuncionarioPorId (@PathVariable Long id) {
+        return funcionarioService.listarFuncionarioPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public FuncionarioResponseDTO atualizarFuncionario (@PathVariable Long id, @RequestBody FuncionarioRequestDTO requestDTO) {
+        return funcionarioService.atualizarFuncionario(id, requestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar (@PathVariable Long id) {
+        funcionarioService.deletarFuncionario(id);
+    }
 }
