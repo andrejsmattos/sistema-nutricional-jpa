c54e0b98d2eb8545b7c8c5327788af3c416510e5
André <andrejusmattos@gmail.com>
2024-06-30 15:13:55 -0300

    Implementação da lógica do NutricionistaController

--- src/main/java/br/senai/lab365/semana7/controllers/NutricionistaController.java
+++ src/main/java/br/senai/lab365/semana7/controllers/NutricionistaController.java
@@ -7,10 +7,9 @@ import br.senai.lab365.semana7.controllers.dtos.NutricionistaResponseDTO;
 import br.senai.lab365.semana7.services.NutricionistaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/nutricionistas")
@@ -23,4 +22,26 @@ public class NutricionistaController {
     public NutricionistaResponseDTO criarNutricionista (@RequestBody NutricionistaRequestDTO requestDTO){
         return nutricionistaService.criarNutricionista(requestDTO);
     }
+
+    @GetMapping
+    public List<NutricionistaResponseDTO> listarTodosNutricionistas () {
+        return nutricionistaService.listarTodosNutricionistas();
+    }
+
+    @GetMapping("/{id}")
+    public NutricionistaResponseDTO listarNutricionistaPorId(@PathVariable Long id){
+        return nutricionistaService.listarNutricionistaPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public NutricionistaResponseDTO atualizarNutricionista (@PathVariable Long id, @RequestBody NutricionistaRequestDTO requestDTO) {
+        return nutricionistaService.atualizarNutricionista(id, requestDTO);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void deletarNutricionista (@PathVariable Long id) {
+        nutricionistaService.deletarNutricionista(id);
+    }
+
 }
