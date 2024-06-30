eb8cd837b6a4f56260368f2b8e0c88cc611251db
André <andrejusmattos@gmail.com>
2024-06-30 18:49:58 -0300

    Implementação da lógica de PacienteController

--- src/main/java/br/senai/lab365/semana7/controllers/PacienteController.java
+++ src/main/java/br/senai/lab365/semana7/controllers/PacienteController.java
@@ -1,18 +1,44 @@
 package br.senai.lab365.semana7.controllers;
 
+import br.senai.lab365.semana7.controllers.dtos.PacienteRequestDTO;
+import br.senai.lab365.semana7.controllers.dtos.PacienteResponseDTO;
+import br.senai.lab365.semana7.repositories.PacienteRepository;
 import br.senai.lab365.semana7.services.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/paciente")
 public class PacienteController {
-//
-//    @Autowired
-//    private PacienteService pacienteService;
-//    public List<PacienteDTO> listarTodos(){
-//
-//    }
+
+    @Autowired
+    private PacienteService pacienteService;
+
+    @PostMapping
+    public PacienteResponseDTO criarPaciente (@RequestBody PacienteRequestDTO requestDTO) {
+        return pacienteService.criarPaciente(requestDTO);
+    }
+
+    @GetMapping
+    public List<PacienteResponseDTO> listarTodosPacientes () {
+        return pacienteService.listarTodosPacientes();
+    }
+
+    @GetMapping("/{id}")
+    public PacienteResponseDTO listarPacientePorId (@PathVariable Long id) {
+        return pacienteService.listarPacientePorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public PacienteResponseDTO atualizarPaciente (@PathVariable Long id, @RequestBody PacienteRequestDTO requestDTO) {
+        return pacienteService.atualizarPaciente(id, requestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarPaciente (@PathVariable Long id) {
+        pacienteService.deletarPaciente(id);
+    }
 }
